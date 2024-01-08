e9211587002824ce02b3ce67b7c1ea82791edd24
lanamoroney <70061041+lanamoroney@users.noreply.github.com>
2024-01-08 10:03:43 +0000

    Update streamlit_app.py
    add second text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,6 @@ my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
+
+second_fruit_choice = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', second_fruit_choice)
